01a2cdc53770b23928cfe13cde48fa4bd82a7cac
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-08-10 23:07:20 -0500

    Solución

--- Ejercicio_Java/src/Auditoria/Auditoria.java
+++ Ejercicio_Java/src/Auditoria/Auditoria.java
@@ -4,7 +4,7 @@ package Auditoria;
 import java.util.Scanner;
 
 public class Auditoria {
-    
+    //Main
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int produccion = sc.nextInt();
